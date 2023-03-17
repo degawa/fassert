1e18d48a0a246e6b25daadeee6af8647b07408e2
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-03-17 23:57:05 +0900

    refactored retval name

--- src/equal/compareArrayValues.f90
+++ src/equal/compareArrayValues.f90
@@ -26,29 +26,29 @@ module assert_equal_compareArrayValues
 contains
     !>二つのスカラ値の差が許容値以下の場合に`.true.`，そうでない場合`.false.`を返す．
     !>許容値が指定されない場合，マシンイプシロンが許容値として使われる．
-    pure function is_approx_equal_real32(val1, val2, tolerance) result(are_same)
+    pure function is_approx_equal_real32(val1, val2, tolerance) result(is_equal)
         use :: assert_common_optval
         implicit none
         real(real32), intent(in) :: val1
         real(real32), intent(in) :: val2
         real(real32), intent(in), optional :: tolerance
-        logical :: are_same
+        logical :: is_equal
 
-        are_same = (abs(val1 - val2) <= optval(tolerance, &
+        is_equal = (abs(val1 - val2) <= optval(tolerance, &
                                                default=epsilon(val1)))
     end function is_approx_equal_real32
 
     !>二つのスカラ値の差が許容値以下の場合に`.true.`，そうでない場合`.false.`を返す．
     !>許容値が指定されない場合，マシンイプシロンが許容値として使われる．
-    pure function is_approx_equal_real64(val1, val2, tolerance) result(are_same)
+    pure function is_approx_equal_real64(val1, val2, tolerance) result(is_equal)
         use :: assert_common_optval
         implicit none
         real(real64), intent(in) :: val1
         real(real64), intent(in) :: val2
         real(real64), intent(in), optional :: tolerance
-        logical :: are_same
+        logical :: is_equal
 
-        are_same = (abs(val1 - val2) <= optval(tolerance, &
+        is_equal = (abs(val1 - val2) <= optval(tolerance, &
                                                default=epsilon(val1)))
     end function is_approx_equal_real64
 
