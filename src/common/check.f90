2d94e638d078b864b2cfbeae1e5c813e6c8cc610
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-03-20 16:53:17 +0900

    added procedure for outputting message to string

--- src/common/check.f90
+++ src/common/check.f90
@@ -9,6 +9,15 @@ module fassert_common_check
     public :: check_true
     public :: check_expected_failure
 
+    interface check_true
+        procedure :: check_true_write_to_unit
+        procedure :: check_true_write_to_string
+    end interface
+
+    interface check_expected_failure
+        procedure :: check_expected_failure_write_to_unit
+        procedure :: check_expected_failure_write_to_string
+    end interface
 contains
     !>`condition`が真かを判別し，
     !>真であれば`PASSED: テスト名`，
@@ -17,7 +26,7 @@ contains
     !>`stat`が存在していれば，真偽を書き込む．
     !>
     !>`quiet`が真の場合，表示を抑制する．
-    subroutine check_true(condition, test_name, stat, quiet)
+    subroutine check_true_write_to_unit(condition, test_name, stat, quiet)
         logical, intent(in) :: condition
             !! 判別される条件
         character(*), intent(in) :: test_name
@@ -31,20 +40,17 @@ contains
             !! 出力を抑制するフラグ
 
         call store(stat, val=condition)
-
         if (does_not_output_message(quiet)) return
 
         block
             character(:), allocatable :: msg_buffer
-            if (condition) then
-                msg_buffer = prefix_passed//test_name
-            else
-                msg_buffer = prefix_failed//test_name
-            end if
-
+            call check_true_write_to_string(condition, test_name, &
+                                            ! stat is not passed
+                                            quiet=quiet, &
+                                            output_message=msg_buffer)
             write (msg_unit, '(A)') msg_buffer
         end block
-    end subroutine check_true
+    end subroutine check_true_write_to_unit
 
     !>`condition`が偽かを判別し，
     !>偽であれば`PASSED: テスト名 [expected failure]`，
@@ -53,7 +59,7 @@ contains
     !>`stat`が存在していれば，真偽（偽の時に真，真の時に偽）を書き込む．
     !>
     !>`quiet`が真の場合，表示を抑制する．
-    subroutine check_expected_failure(condition, test_name, stat, quiet)
+    subroutine check_expected_failure_write_to_unit(condition, test_name, stat, quiet)
         use :: fassert_common_message
         logical, intent(in) :: condition
             !! 判別される条件
@@ -69,6 +75,86 @@ contains
             !! 出力を抑制するフラグ
 
         call store(stat, val=.not. condition)
+        if (does_not_output_message(quiet)) return
+
+        block
+            character(:), allocatable :: msg_buffer
+            call check_expected_failure_write_to_string(condition, test_name, &
+                                                        ! stat is not passed
+                                                        quiet=quiet, &
+                                                        output_message=msg_buffer)
+            write (msg_unit, '(A)') msg_buffer
+        end block
+    end subroutine check_expected_failure_write_to_unit
+
+    !>`condition`が真かを判別し，
+    !>真であれば`PASSED: テスト名`，
+    !>偽であれば`FAILED: テスト名`を`output_message`に書き込む．
+    !>
+    !>`stat`が存在していれば，真偽を書き込む．
+    !>
+    !>`quiet`が真の場合，`output_message`を割り付けず，何も書き込まない．
+    subroutine check_true_write_to_string(condition, test_name, stat, &
+                                          quiet, output_message)
+        logical, intent(in) :: condition
+            !! 判別される条件
+        character(*), intent(in) :: test_name
+            !! テスト名
+        logical, intent(out), optional :: stat
+            !! 条件`condition`の真偽値<br>
+            !! `condition`が真の場合`.true.`，
+            !! そうでない場合`.false.`
+        logical, intent(in), optional :: quiet
+            !! `condition`が真の場合に
+            !! 出力を抑制するフラグ
+        character(:), allocatable, intent(out) :: output_message
+            !! 結果を格納する文字列<br>
+            !! 追記ではないので，`intent(out)`でよい．
+
+        call store(stat, val=condition)
+
+        if (does_not_output_message(quiet)) return
+
+        block
+            character(:), allocatable :: msg_buffer
+            if (condition) then
+                msg_buffer = prefix_passed//test_name
+            else
+                msg_buffer = prefix_failed//test_name
+            end if
+
+            output_message = msg_buffer
+        end block
+    end subroutine check_true_write_to_string
+
+    !>`condition`が偽かを判別し，
+    !>偽であれば`PASSED: テスト名 [expected failure]`，
+    !>真であれば`FAILED: テスト名 [unexpected success]`を
+    !>`output_message`に書き込む．
+    !>
+    !>`stat`が存在していれば，真偽（偽の時に真，真の時に偽）を書き込む．
+    !>
+    !>`quiet`が真の場合，`output_message`を割り付けず，何も書き込まない．
+    subroutine check_expected_failure_write_to_string(condition, test_name, stat, &
+                                                      quiet, output_message)
+        use :: fassert_common_message
+        logical, intent(in) :: condition
+            !! 判別される条件
+        character(*), intent(in) :: test_name
+            !! テスト名
+        logical, intent(out), optional :: stat
+            !! 条件`condition`の真偽値<br>
+            !! `expected_failure`が偽，`condition`が真の場合`.true.`，
+            !! `expected_failure`が真，`condition`が偽の場合`.true.`，
+            !! それ以外の場合`.false.`
+        logical, intent(in), optional :: quiet
+            !! `condition`が偽の場合に
+            !! 出力を抑制するフラグ
+        character(:), allocatable, intent(out) :: output_message
+            !! 結果を格納する文字列<br>
+            !! 追記ではないので，`intent(out)`でよい．
+
+        call store(stat, val=.not. condition)
 
         if (does_not_output_message(quiet)) return
 
@@ -80,7 +166,7 @@ contains
                 msg_buffer = prefix_failed//test_name//note_unexpected_pass
             end if
 
-            write (msg_unit, '(A)') msg_buffer
+            output_message = msg_buffer
         end block
-    end subroutine check_expected_failure
+    end subroutine check_expected_failure_write_to_string
 end module fassert_common_check
