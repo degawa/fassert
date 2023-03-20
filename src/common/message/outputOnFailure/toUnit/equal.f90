50f7441f8f14aea63abd3d9397dda96a8874a0ff
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-03-20 19:25:48 +0900

    refactored procedures for vorbose output to unit

--- src/common/message/outputOnFailure/toUnit/equal.f90
+++ src/common/message/outputOnFailure/toUnit/equal.f90
@@ -2,6 +2,8 @@ module fassert_common_message_outputOnFailure_toUnit_equal
     use, intrinsic :: iso_fortran_env
     use :: fassert_common_unit, msg_unit => assertion_message_unit
     use :: fassert_common_message, only:default_verbose_format_indent
+    use :: fassert_common_message_outputOnFailure_format
+    use :: fassert_common_message_outputOnFailure_toString_equal
     implicit none
     private
     public :: output_on_failure
@@ -27,18 +29,8 @@ module fassert_common_message_outputOnFailure_toUnit_equal
         procedure :: output_char_rank1
     end interface
 
-    character(*), private, parameter :: fmt_indent = default_verbose_format_indent
-    character(*), private, parameter :: fmt_int = '('//fmt_indent//',A,I0)'
-    character(*), private, parameter :: fmt_real = '('//fmt_indent//',A,g0)'
-    character(*), private, parameter :: fmt_str = '('//fmt_indent//',A,A)'
-    character(*), private, parameter :: fmt_char = '('//fmt_indent//',A,*(A:," "))'
-
-    character(*), private, parameter :: fmt_idx_sep = ',",",'
-    character(*), private, parameter :: fmt_idx_d1 = 'i0'
-    character(*), private, parameter :: fmt_idx_d2 = fmt_idx_d1//repeat(fmt_idx_sep//fmt_idx_d1, 2 - 1)
-    character(*), private, parameter :: fmt_idx_d3 = fmt_idx_d1//repeat(fmt_idx_sep//fmt_idx_d1, 3 - 1)
 contains
-    !>実測値と予測値を標準出力に出力する．
+    !>実測値と予測値を装置に出力する．
     subroutine output_int8(actual, expected)
         implicit none
         integer(int8), intent(in) :: actual
@@ -46,11 +38,12 @@ contains
         integer(int8), intent(in) :: expected
             !! 予測値
 
-        write (msg_unit, fmt_int) "Expected: ", expected
-        write (msg_unit, fmt_int) "Actual  : ", actual
+        character(:), allocatable :: msg
+        call output_on_failure(actual, expected, msg)
+        write (msg_unit, '(A)') msg
     end subroutine output_int8
 
-    !>実測値と予測値を標準出力に出力する．
+    !>実測値と予測値を装置に出力する．
     subroutine output_int16(actual, expected)
         implicit none
         integer(int16), intent(in) :: actual
@@ -58,11 +51,12 @@ contains
         integer(int16), intent(in) :: expected
             !! 予測値
 
-        write (msg_unit, fmt_int) "Expected: ", expected
-        write (msg_unit, fmt_int) "Actual  : ", actual
+        character(:), allocatable :: msg
+        call output_on_failure(actual, expected, msg)
+        write (msg_unit, '(A)') msg
     end subroutine output_int16
 
-    !>実測値と予測値を標準出力に出力する．
+    !>実測値と予測値を装置に出力する．
     subroutine output_int32(actual, expected)
         implicit none
         integer(int32), intent(in) :: actual
@@ -70,11 +64,12 @@ contains
         integer(int32), intent(in) :: expected
             !! 予測値
 
-        write (msg_unit, fmt_int) "Expected: ", expected
-        write (msg_unit, fmt_int) "Actual  : ", actual
+        character(:), allocatable :: msg
+        call output_on_failure(actual, expected, msg)
+        write (msg_unit, '(A)') msg
     end subroutine output_int32
 
-    !>実測値と予測値を標準出力に出力する．
+    !>実測値と予測値を装置に出力する．
     subroutine output_int64(actual, expected)
         implicit none
         integer(int64), intent(in) :: actual
@@ -82,11 +77,12 @@ contains
         integer(int64), intent(in) :: expected
             !! 予測値
 
-        write (msg_unit, fmt_int) "Expected: ", expected
-        write (msg_unit, fmt_int) "Actual  : ", actual
+        character(:), allocatable :: msg
+        call output_on_failure(actual, expected, msg)
+        write (msg_unit, '(A)') msg
     end subroutine output_int64
 
-    !>実測値と予測値，それらの差を標準出力に出力する．
+    !>実測値と予測値，それらの差を装置に出力する．
     subroutine output_real32(actual, expected)
         implicit none
         real(real32), intent(in) :: actual
@@ -94,12 +90,12 @@ contains
         real(real32), intent(in) :: expected
             !! 予測値
 
-        write (msg_unit, fmt_real) "Expected: ", expected
-        write (msg_unit, fmt_real) "Actual  : ", actual
-        write (msg_unit, fmt_real) "Difference: ", expected - actual
+        character(:), allocatable :: msg
+        call output_on_failure(actual, expected, msg)
+        write (msg_unit, '(A)') msg
     end subroutine output_real32
 
-    !>実測値と予測値，それらの差を標準出力に出力する．
+    !>実測値と予測値，それらの差を装置に出力する．
     subroutine output_real64(actual, expected)
         implicit none
         real(real64), intent(in) :: actual
@@ -107,12 +103,12 @@ contains
         real(real64), intent(in) :: expected
             !! 予測値
 
-        write (msg_unit, fmt_real) "Expected: ", expected
-        write (msg_unit, fmt_real) "Actual  : ", actual
-        write (msg_unit, fmt_real) "Difference:", expected - actual
+        character(:), allocatable :: msg
+        call output_on_failure(actual, expected, msg)
+        write (msg_unit, '(A)') msg
     end subroutine output_real64
 
-    !>実測値と予測値の差の最大・最小値を標準出力に出力する．
+    !>実測値と予測値の差の最大・最小値を装置に出力する．
     subroutine output_int32_rank1(actual, expected)
         implicit none
         integer(int32), intent(in) :: actual(:)
@@ -120,17 +116,12 @@ contains
         integer(int32), intent(in) :: expected(:)
             !! 予測値
 
-        character(*), parameter :: fmt = '('//fmt_indent//',A,i0," at (",'//fmt_idx_d1//',")")'
-
-        write (msg_unit, fmt) "Maximum Absolute Difference: ", &
-                                maxval(abs(expected - actual)), &
-                                findloc(abs(expected - actual), maxval(abs(expected - actual))) !&
-        write (msg_unit, fmt) "Minimum Absolute Difference: ", &
-                                minval(abs(expected - actual)), &
-                                findloc(abs(expected - actual), minval(abs(expected - actual))) !&
+        character(:), allocatable :: msg
+        call output_on_failure(actual, expected, msg)
+        write (msg_unit, '(A)') msg
     end subroutine output_int32_rank1
 
-    !>実測値と予測値の差の最大・最小値を標準出力に出力する．
+    !>実測値と予測値の差の最大・最小値を装置に出力する．
     subroutine output_int32_rank2(actual, expected)
         implicit none
         integer(int32), intent(in) :: actual(:, :)
@@ -138,17 +129,12 @@ contains
         integer(int32), intent(in) :: expected(:, :)
             !! 予測値
 
-        character(*), parameter :: fmt = '('//fmt_indent//',A,i0," at (",'//fmt_idx_d2//',")")'
-
-        write (msg_unit, fmt) "Maximum Absolute Difference: ", &
-                                maxval(abs(expected - actual)), &
-                                findloc(abs(expected - actual), maxval(abs(expected - actual))) !&
-        write (msg_unit, fmt) "Minimum Absolute Difference: ", &
-                                minval(abs(expected - actual)), &
-                                findloc(abs(expected - actual), minval(abs(expected - actual))) !&
+        character(:), allocatable :: msg
+        call output_on_failure(actual, expected, msg)
+        write (msg_unit, '(A)') msg
     end subroutine output_int32_rank2
 
-    !>実測値と予測値の差の最大・最小値を標準出力に出力する．
+    !>実測値と予測値の差の最大・最小値を装置に出力する．
     subroutine output_int32_rank3(actual, expected)
         implicit none
         integer(int32), intent(in) :: actual(:, :, :)
@@ -156,17 +142,12 @@ contains
         integer(int32), intent(in) :: expected(:, :, :)
             !! 予測値
 
-        character(*), parameter :: fmt = '('//fmt_indent//',A,i0," at (",'//fmt_idx_d3//',")")'
-
-        write (msg_unit, fmt) "Maximum Absolute Difference: ", &
-                                maxval(abs(expected - actual)), &
-                                findloc(abs(expected - actual), maxval(abs(expected - actual))) !&
-        write (msg_unit, fmt) "Minimum Absolute Difference: ", &
-                                minval(abs(expected - actual)), &
-                                findloc(abs(expected - actual), minval(abs(expected - actual))) !&
+        character(:), allocatable :: msg
+        call output_on_failure(actual, expected, msg)
+        write (msg_unit, '(A)') msg
     end subroutine output_int32_rank3
 
-    !>実測値と予測値の差の最大・最小値を標準出力に出力する．
+    !>実測値と予測値の差の最大・最小値を装置に出力する．
     subroutine output_real32_rank1(actual, expected)
         implicit none
         real(real32), intent(in) :: actual(:)
@@ -174,17 +155,12 @@ contains
         real(real32), intent(in) :: expected(:)
             !! 予測値
 
-        character(*), parameter :: fmt = '('//fmt_indent//',A,g0," at (",'//fmt_idx_d1//',")")'
-
-        write (msg_unit, fmt) "Maximum Absolute Difference: ", &
-                                maxval(abs(expected - actual)), &
-                                findloc(abs(expected - actual), maxval(abs(expected - actual))) !&
-        write (msg_unit, fmt) "Minimum Absolute Difference: ", &
-                                minval(abs(expected - actual)), &
-                                findloc(abs(expected - actual), minval(abs(expected - actual))) !&
+        character(:), allocatable :: msg
+        call output_on_failure(actual, expected, msg)
+        write (msg_unit, '(A)') msg
     end subroutine output_real32_rank1
 
-    !>実測値と予測値の差の最大・最小値を標準出力に出力する．
+    !>実測値と予測値の差の最大・最小値を装置に出力する．
     subroutine output_real32_rank2(actual, expected)
         implicit none
         real(real32), intent(in) :: actual(:, :)
@@ -192,17 +168,12 @@ contains
         real(real32), intent(in) :: expected(:, :)
             !! 予測値
 
-        character(*), parameter :: fmt = '('//fmt_indent//',A,g0," at (",'//fmt_idx_d2//',")")'
-
-        write (msg_unit, fmt) "Maximum Absolute Difference: ", &
-                                maxval(abs(expected - actual)), &
-                                findloc(abs(expected - actual), maxval(abs(expected - actual))) !&
-        write (msg_unit, fmt) "Minimum Absolute Difference: ", &
-                                minval(abs(expected - actual)), &
-                                findloc(abs(expected - actual), minval(abs(expected - actual))) !&
+        character(:), allocatable :: msg
+        call output_on_failure(actual, expected, msg)
+        write (msg_unit, '(A)') msg
     end subroutine output_real32_rank2
 
-    !>実測値と予測値の差の最大・最小値を標準出力に出力する．
+    !>実測値と予測値の差の最大・最小値を装置に出力する．
     subroutine output_real32_rank3(actual, expected)
         implicit none
         real(real32), intent(in) :: actual(:, :, :)
@@ -210,17 +181,12 @@ contains
         real(real32), intent(in) :: expected(:, :, :)
             !! 予測値
 
-        character(*), parameter :: fmt = '('//fmt_indent//',A,g0," at (",'//fmt_idx_d3//',")")'
-
-        write (msg_unit, fmt) "Maximum Absolute Difference: ", &
-                                maxval(abs(expected - actual)), &
-                                findloc(abs(expected - actual), maxval(abs(expected - actual))) !&
-        write (msg_unit, fmt) "Minimum Absolute Difference: ", &
-                                minval(abs(expected - actual)), &
-                                findloc(abs(expected - actual), minval(abs(expected - actual))) !&
+        character(:), allocatable :: msg
+        call output_on_failure(actual, expected, msg)
+        write (msg_unit, '(A)') msg
     end subroutine output_real32_rank3
 
-    !>実測値と予測値の差の最大・最小値を標準出力に出力する．
+    !>実測値と予測値の差の最大・最小値を装置に出力する．
     subroutine output_real64_rank1(actual, expected)
         implicit none
         real(real64), intent(in) :: actual(:)
@@ -228,17 +194,14 @@ contains
         real(real64), intent(in) :: expected(:)
             !! 予測値
 
-        character(*), parameter :: fmt = '('//fmt_indent//',A,g0," at (",'//fmt_idx_d1//',")")'
+        character(*), parameter :: fmt = '('//fmt_indent//',A,g0," at (",'//fmt_index_rank1//',")")'
 
-        write (msg_unit, fmt) "Maximum Absolute Difference: ", &
-                                maxval(abs(expected - actual)), &
-                                findloc(abs(expected - actual), maxval(abs(expected - actual))) !&
-        write (msg_unit, fmt) "Minimum Absolute Difference: ", &
-                                minval(abs(expected - actual)), &
-                                findloc(abs(expected - actual), minval(abs(expected - actual))) !&
+        character(:), allocatable :: msg
+        call output_on_failure(actual, expected, msg)
+        write (msg_unit, '(A)') msg
     end subroutine output_real64_rank1
 
-    !>実測値と予測値の差の最大・最小値を標準出力に出力する．
+    !>実測値と予測値の差の最大・最小値を装置に出力する．
     subroutine output_real64_rank2(actual, expected)
         implicit none
         real(real64), intent(in) :: actual(:, :)
@@ -246,17 +209,14 @@ contains
         real(real64), intent(in) :: expected(:, :)
             !! 予測値
 
-        character(*), parameter :: fmt = '('//fmt_indent//',A,g0," at (",'//fmt_idx_d2//',")")'
+        character(*), parameter :: fmt = '('//fmt_indent//',A,g0," at (",'//fmt_index_rank2//',")")'
 
-        write (msg_unit, fmt) "Maximum Absolute Difference: ", &
-                                maxval(abs(expected - actual)), &
-                                findloc(abs(expected - actual), maxval(abs(expected - actual))) !&
-        write (msg_unit, fmt) "Minimum Absolute Difference: ", &
-                                minval(abs(expected - actual)), &
-                                findloc(abs(expected - actual), minval(abs(expected - actual))) !&
+        character(:), allocatable :: msg
+        call output_on_failure(actual, expected, msg)
+        write (msg_unit, '(A)') msg
     end subroutine output_real64_rank2
 
-    !>実測値と予測値の差の最大・最小値を標準出力に出力する．
+    !>実測値と予測値の差の最大・最小値を装置に出力する．
     subroutine output_real64_rank3(actual, expected)
         implicit none
         real(real64), intent(in) :: actual(:, :, :)
@@ -264,17 +224,14 @@ contains
         real(real64), intent(in) :: expected(:, :, :)
             !! 予測値
 
-        character(*), parameter :: fmt = '('//fmt_indent//',A,g0," at (",'//fmt_idx_d3//',")")'
+        character(*), parameter :: fmt = '('//fmt_indent//',A,g0," at (",'//fmt_index_rank3//',")")'
 
-        write (msg_unit, fmt) "Maximum Absolute Difference: ", &
-                                maxval(abs(expected - actual)), &
-                                findloc(abs(expected - actual), maxval(abs(expected - actual))) !&
-        write (msg_unit, fmt) "Minimum Absolute Difference: ", &
-                                minval(abs(expected - actual)), &
-                                findloc(abs(expected - actual), minval(abs(expected - actual))) !&
+        character(:), allocatable :: msg
+        call output_on_failure(actual, expected, msg)
+        write (msg_unit, '(A)') msg
     end subroutine output_real64_rank3
 
-    !>実測値と予測値を標準出力に出力する．
+    !>実測値と予測値を装置に出力する．
     subroutine output_logical(actual, expected)
         use :: fassert_common_message, only:to_string
         implicit none
@@ -283,11 +240,12 @@ contains
         logical, intent(in) :: expected
             !! 予測値
 
-        write (msg_unit, fmt_str) "Expected: ", to_string(expected)
-        write (msg_unit, fmt_str) "Actual  : ", to_string(actual)
+        character(:), allocatable :: msg
+        call output_on_failure(actual, expected, msg)
+        write (msg_unit, '(A)') msg
     end subroutine output_logical
 
-    !>実測値と予測値を標準出力に出力する．
+    !>実測値と予測値を装置に出力する．
     subroutine output_string(actual, expected)
         implicit none
         character(*), intent(in) :: actual
@@ -295,11 +253,12 @@ contains
         character(*), intent(in) :: expected
             !! 予測値
 
-        write (msg_unit, fmt_str) "Expected: ", expected
-        write (msg_unit, fmt_str) "Actual  : ", actual
+        character(:), allocatable :: msg
+        call output_on_failure(actual, expected, msg)
+        write (msg_unit, '(A)') msg
     end subroutine output_string
 
-    !>実測値と予測値を標準出力に出力する．
+    !>実測値と予測値を装置に出力する．
     subroutine output_char_rank1(actual, expected)
         implicit none
         character, intent(in) :: actual(:)
@@ -307,7 +266,8 @@ contains
         character, intent(in) :: expected(:)
             !! 予測値
 
-        write (msg_unit, fmt_char) "Expected: ", expected
-        write (msg_unit, fmt_char) "Actual  : ", actual
+        character(:), allocatable :: msg
+        call output_on_failure(actual, expected, msg)
+        write (msg_unit, '(A)') msg
     end subroutine output_char_rank1
 end module fassert_common_message_outputOnFailure_toUnit_equal
