62404478ca27bf502b108db4db054273f9e0453b
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-03-20 18:43:43 +0900

    refactored `expect_true` and `expect_false`
    using submodule

--- src/logical/expectLogical.f90
+++ src/logical/expectLogical.f90
@@ -8,88 +8,70 @@ module expectLogical
     public :: expect_true
     public :: expect_false
 
-contains
-    !>実測値`actual`が真であるか検査する．
-    !>
-    !>検査結果を`stat`に書き込む．
-    !>
-    !>`verbose`が真であれば，実測値と予測値を出力する．
-    !>
-    !>`expected_failure`が真の場合，実測値が偽であることを検査する．
-    !>
-    !>`quiet`が真の場合，出力を抑制する．
-    !>
-    subroutine expect_true(actual, test_name, stat, &
-                           verbose, expected_failure, quiet)
-        use :: fassert_common_message_outputOnFailure_equal
-        implicit none
-        logical, intent(in) :: actual
+    interface expect_true
+        procedure :: expect_true_
+    end interface
+
+    interface expect_false
+        procedure :: expect_false_
+    end interface
+
+    interface
+        !>実測値`actual`が真であるか検査する．
+        !>
+        !>検査結果を`stat`に書き込む．
+        !>
+        !>`verbose`が真であれば，実測値と予測値を出力する．
+        !>
+        !>`expected_failure`が真の場合，実測値が偽であることを検査する．
+        !>
+        !>`quiet`が真の場合，出力を抑制する．
+        !>
+        module subroutine expect_true_(actual, test_name, stat, &
+                                       verbose, expected_failure, quiet)
+            logical, intent(in) :: actual
             !! 実測値
-        character(*), intent(in) :: test_name
+            character(*), intent(in) :: test_name
             !! テスト名
-        logical, intent(out) :: stat
+            logical, intent(out) :: stat
             !! 検査結果の真偽値<br>
             !! 実測値が`.true.`の場合`.true.`，
             !! そうでない場合`.false.`
-        logical, intent(in), optional :: verbose
+            logical, intent(in), optional :: verbose
             !! 実測値と予測値を出力するフラグ
-        logical, intent(in), optional :: expected_failure
+            logical, intent(in), optional :: expected_failure
             !! 予期された失敗を検査するかのフラグ
-        logical, intent(in), optional :: quiet
+            logical, intent(in), optional :: quiet
             !! 出力を抑制するかのフラグ
+        end subroutine expect_true_
 
-        logical :: expected
-
-        if (is_test_of_expected_failure(expected_failure)) then
-            expected = .false.
-            call check_expected_failure(actual, test_name, stat, quiet)
-        else
-            expected = .true.
-            call check_true(actual, test_name, stat, quiet)
-        end if
-
-        if (is_verbose_output(stat, verbose, quiet)) call output_on_failure(actual, expected)
-    end subroutine expect_true
-
-    !>実測値`actual`が偽であるか検査する．
-    !>
-    !>検査結果を`stat`に書き込む．
-    !>
-    !>`verbose`が真であれば，実測値と予測値を出力する．
-    !>
-    !>`expected_failure`が真の場合，実測値が真であることを検査する．
-    !>
-    !>`quiet`が真の場合，出力を抑制する．
-    !>
-    subroutine expect_false(actual, test_name, stat, &
-                            verbose, expected_failure, quiet)
-        use :: fassert_common_message_outputOnFailure_equal
-        implicit none
-        logical, intent(in) :: actual
+        !>実測値`actual`が偽であるか検査する．
+        !>
+        !>検査結果を`stat`に書き込む．
+        !>
+        !>`verbose`が真であれば，実測値と予測値を出力する．
+        !>
+        !>`expected_failure`が真の場合，実測値が真であることを検査する．
+        !>
+        !>`quiet`が真の場合，出力を抑制する．
+        !>
+        module subroutine expect_false_(actual, test_name, stat, &
+                                        verbose, expected_failure, quiet)
+            logical, intent(in) :: actual
             !! 実測値
-        character(*), intent(in) :: test_name
+            character(*), intent(in) :: test_name
             !! テスト名
-        logical, intent(out) :: stat
+            logical, intent(out) :: stat
             !! 検査結果の真偽値<br>
             !! 実測値が`.false.`の場合`.true.`，
             !! そうでない場合`.false.`
-        logical, intent(in), optional :: verbose
+            logical, intent(in), optional :: verbose
             !! 実測値と予測値を出力するフラグ
-        logical, intent(in), optional :: expected_failure
+            logical, intent(in), optional :: expected_failure
             !! 予期された失敗を検査するかのフラグ
-        logical, intent(in), optional :: quiet
+            logical, intent(in), optional :: quiet
             !! 出力を抑制するかのフラグ
+        end subroutine expect_false_
+    end interface
 
-        logical :: expected
-
-        if (is_test_of_expected_failure(expected_failure)) then
-            expected = .true.
-            call check_expected_failure(.not. actual, test_name, stat, quiet)
-        else
-            expected = .false.
-            call check_true(.not. actual, test_name, stat, quiet)
-        end if
-
-        if (is_verbose_output(stat, verbose, quiet)) call output_on_failure(actual, expected)
-    end subroutine expect_false
 end module expectLogical
