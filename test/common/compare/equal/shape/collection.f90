53f8e7fff6d0a00cefe15e8a064e436a348a38a3
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-03-20 18:26:14 +0900

    refactored test collection for `are_same_shape`

--- test/common/compare/equal/shape/collection.f90
+++ test/common/compare/equal/shape/collection.f90
@@ -14,59 +14,80 @@ contains
             !! collection of tests
 
         test_suite = [ &
-                     new_unittest("are_same_shape_rank1_int32(), it should return .true. when input arrays having same shape.", &
+                     new_unittest("are_same_shape_rank1() for int32, "// &
+                                  "it should return .true. when input arrays having same shape.", &
                                   d1_i32_should_return_true_when_input_same_shape) &
-                     , new_unittest("are_same_shape_rank1_int32(), it should return .false. "// &
+                     , new_unittest("are_same_shape_rank1() for int32, "// &
+                                    "it should return .false. "// &
                                     "when input arrays having different shape.", &
                                     d1_i32_should_return_false_when_input_different_shape) &
-                     , new_unittest("are_same_shape_rank2_int32(), it should return .true. when input arrays having same shape.", &
+                     , new_unittest("are_same_shape_rank2() for int32, "// &
+                                    "it should return .true. when input arrays having same shape.", &
                                     d2_i32_should_return_true_when_input_same_shape) &
-                     , new_unittest("are_same_shape_rank2_int32(), it should return .false. "// &
+                     , new_unittest("are_same_shape_rank2() for int32, "// &
+                                    "it should return .false. "// &
                                     "when input arrays having different shape.", &
                                     d2_i32_should_return_false_when_input_different_shape) &
-                     , new_unittest("are_same_shape_rank3_int32(), it should return .true. when input arrays having same shape.", &
+                     , new_unittest("are_same_shape_rank3() for int32, "// &
+                                    "it should return .true. when input arrays having same shape.", &
                                     d3_i32_should_return_true_when_input_same_shape) &
-                     , new_unittest("are_same_shape_rank3_int32(), it should return .false. "// &
+                     , new_unittest("are_same_shape_rank3() for int32, it should return .false. "// &
                                     "when input arrays having different shape.", &
                                     d3_i32_should_return_false_when_input_different_shape) &
-                     , new_unittest("are_same_shape_rank1_real32(), it should return .true. when input arrays having same shape.", &
+                     , new_unittest("are_same_shape_rank1() for real32, "// &
+                                    "it should return .true. when input arrays having same shape.", &
                                     d1_r32_should_return_true_when_input_same_shape) &
-                     , new_unittest("are_same_shape_rank1_real32(), it should return .false. "// &
+                     , new_unittest("are_same_shape_rank1() for real32, "// &
+                                    "it should return .false. "// &
                                     "when input arrays having different shape.", &
                                     d1_r32_should_return_false_when_input_different_shape) &
-                     , new_unittest("are_same_shape_rank2_real32(), it should return .true. when input arrays having same shape.", &
+                     , new_unittest("are_same_shape_rank2() for real32, "// &
+                                    "it should return .true. when input arrays having same shape.", &
                                     d2_r32_should_return_true_when_input_same_shape) &
-                     , new_unittest("are_same_shape_rank2_real32(), it should return .false. "// &
+                     , new_unittest("are_same_shape_rank2() for real32, "// &
+                                    "it should return .false. "// &
                                     "when input arrays having different shape.", &
                                     d2_r32_should_return_false_when_input_different_shape) &
-                     , new_unittest("are_same_shape_rank3_real32(), it should return .true. when input arrays having same shape.", &
+                     , new_unittest("are_same_shape_rank3() for real32, "// &
+                                    "it should return .true. when input arrays having same shape.", &
                                     d3_r32_should_return_true_when_input_same_shape) &
-                     , new_unittest("are_same_shape_rank3_real32(), it should return .false. "// &
+                     , new_unittest("are_same_shape_rank3() for real32, "// &
+                                    "it should return .false. "// &
                                     "when input arrays having different shape.", &
                                     d3_r32_should_return_false_when_input_different_shape) &
-                     , new_unittest("are_same_shape_rank1_real32(), it should return .true. when input arrays having same shape.", &
+                     , new_unittest("are_same_shape_rank1() for real32, "// &
+                                    "it should return .true. when input arrays having same shape.", &
                                     d1_r32_should_return_true_when_input_same_shape) &
-                     , new_unittest("are_same_shape_rank1_real64(), it should return .true. when input arrays having same shape.", &
+                     , new_unittest("are_same_shape_rank1() for real64, "// &
+                                    "it should return .true. when input arrays having same shape.", &
                                     d1_r64_should_return_true_when_input_same_shape) &
-                     , new_unittest("are_same_shape_rank1_real64(), it should return .false. "// &
+                     , new_unittest("are_same_shape_rank1() for real64, "// &
+                                    "it should return .false. "// &
                                     "when input arrays having different shape.", &
                                     d1_r64_should_return_false_when_input_different_shape) &
-                     , new_unittest("are_same_shape_rank2_real64(), it should return .true. when input arrays having same shape.", &
+                     , new_unittest("are_same_shape_rank2() for real64, "// &
+                                    "it should return .true. when input arrays having same shape.", &
                                     d2_r64_should_return_true_when_input_same_shape) &
-                     , new_unittest("are_same_shape_rank2_real64(), it should return .false. "// &
+                     , new_unittest("are_same_shape_rank2() for real64, "// &
+                                    "it should return .false. "// &
                                     "when input arrays having different shape.", &
                                     d2_r64_should_return_false_when_input_different_shape) &
-                     , new_unittest("are_same_shape_rank3_real64(), it should return .true. when input arrays having same shape.", &
+                     , new_unittest("are_same_shape_rank3() for real64, "// &
+                                    "it should return .true. when input arrays having same shape.", &
                                     d3_r64_should_return_true_when_input_same_shape) &
-                     , new_unittest("are_same_shape_rank3_real64(), it should return .false. "// &
+                     , new_unittest("are_same_shape_rank3() for real64, "// &
+                                    "it should return .false. "// &
                                     "when input arrays having different shape.", &
                                     d3_r64_should_return_false_when_input_different_shape) &
-                     , new_unittest("are_same_length_str(), it should return .true. when input strings having same length.", &
+                     , new_unittest("are_same_length_str(), "// &
+                                    "it should return .true. when input strings having same length.", &
                                     str_should_return_true_when_input_same_length_string) &
-                     , new_unittest("are_same_length_str(), it should return .false. "// &
+                     , new_unittest("are_same_length_str(), "// &
+                                    "it should return .false. "// &
                                     "when input strings having different length.", &
                                     str_should_return_false_when_input_different_length_string) &
-                     , new_unittest("are_same_length_str(), it should return .true. "// &
+                     , new_unittest("are_same_length_str(), "// &
+                                    "it should return .true. "// &
                                     "when input strings having different length due to trailing whitespace.", &
                                     str_should_return_true_when_input_diff_len_due_to_tailing_space) &
                      ]
