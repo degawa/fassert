301cf97c6b4c69cb215fdc45f07fa20639eefd31
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-03-21 11:50:02 +0900

    added `expect_same_shape` that output to string

--- src/sameShape/expectSameShape.f90
+++ src/sameShape/expectSameShape.f90
@@ -9,6 +9,10 @@ module expectSameShape
         procedure :: expect_same_shape_rank1
         procedure :: expect_same_shape_rank2
         procedure :: expect_same_shape_rank3
+
+        procedure :: expect_same_shape_rank1_msg
+        procedure :: expect_same_shape_rank2_msg
+        procedure :: expect_same_shape_rank3_msg
     end interface
 
     interface
@@ -80,5 +84,82 @@ module expectSameShape
             logical, intent(in), optional :: quiet
                 !! 表示を抑制するかのフラグ
         end subroutine expect_same_shape_rank3
+    end interface
+
+    interface
+        !>二つの配列が同じ形状かを検査し，出力を`output_message`に書き込む．
+        !>
+        !>検査結果を`stat`に書き込む．
+        !>
+        !>`quiet`が真の場合，表示を抑制する．
+        module subroutine expect_same_shape_rank1_msg(actual, expected, test_name, stat, &
+                                                      verbose, quiet, output_message)
+            class(*), intent(in) :: actual(:)
+                !! 実測値
+            class(*), intent(in) :: expected(:)
+                !! 予測値
+            character(*), intent(in) :: test_name
+                !! テスト名
+            logical, intent(out) :: stat
+                !! 比較結果の真偽値<br>
+                !! 実測値と予測値の配列形状が同じの場合`.true.`，
+                !! そうでない場合`.false.`
+            logical, intent(in), optional :: verbose
+                !! 実測値と予測値の配列形状を出力するフラグ
+            logical, intent(in), optional :: quiet
+                !! 表示を抑制するかのフラグ
+            character(:), allocatable, intent(out) :: output_message
+                !! 出力を格納する文字列
+        end subroutine expect_same_shape_rank1_msg
+
+        !>二つの配列が同じ形状かを検査し，出力を`output_message`に書き込む．
+        !>
+        !>検査結果を`stat`に書き込む．
+        !>
+        !>`quiet`が真の場合，表示を抑制する．
+        module subroutine expect_same_shape_rank2_msg(actual, expected, test_name, stat, &
+                                                      verbose, quiet, output_message)
+            class(*), intent(in) :: actual(:, :)
+                !! 実測値
+            class(*), intent(in) :: expected(:, :)
+                !! 予測値
+            character(*), intent(in) :: test_name
+                !! テスト名
+            logical, intent(out) :: stat
+                !! 比較結果の真偽値<br>
+                !! 実測値と予測値の配列形状が同じの場合`.true.`，
+                !! そうでない場合`.false.`
+            logical, intent(in), optional :: verbose
+                !! 実測値と予測値の配列形状を出力するフラグ
+            logical, intent(in), optional :: quiet
+                !! 表示を抑制するかのフラグ
+            character(:), allocatable, intent(out) :: output_message
+                !! 出力を格納する文字列
+        end subroutine expect_same_shape_rank2_msg
+
+        !>二つの配列が同じ形状かを検査し，出力を`output_message`に書き込む．
+        !>
+        !>検査結果を`stat`に書き込む．
+        !>
+        !>`quiet`が真の場合，表示を抑制する．
+        module subroutine expect_same_shape_rank3_msg(actual, expected, test_name, stat, &
+                                                      verbose, quiet, output_message)
+            class(*), intent(in) :: actual(:, :, :)
+                !! 実測値
+            class(*), intent(in) :: expected(:, :, :)
+                !! 予測値
+            character(*), intent(in) :: test_name
+                !! テスト名
+            logical, intent(out) :: stat
+                !! 比較結果の真偽値<br>
+                !! 実測値と予測値の配列形状が同じの場合`.true.`，
+                !! そうでない場合`.false.`
+            logical, intent(in), optional :: verbose
+                !! 実測値と予測値の配列形状を出力するフラグ
+            logical, intent(in), optional :: quiet
+                !! 表示を抑制するかのフラグ
+            character(:), allocatable, intent(out) :: output_message
+                !! 出力を格納する文字列
+        end subroutine expect_same_shape_rank3_msg
     end interface
 end module expectSameShape
