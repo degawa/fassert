adbcec5368c23903a1cfb3f999327be0ef175f83
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-03-22 10:52:57 +0900

    added assertion for user-defined array up to rank3

--- src/equal/assertEqual.f90
+++ src/equal/assertEqual.f90
@@ -26,6 +26,9 @@ module assertEqual
         procedure :: assert_equal_str
         procedure :: assert_equal_char_rank1
         procedure :: assert_equal_user_defined
+        procedure :: assert_equal_user_defined_rank1
+        procedure :: assert_equal_user_defined_rank2
+        procedure :: assert_equal_user_defined_rank3
     end interface
 
 contains
@@ -685,4 +688,130 @@ contains
                           verbose, expected_failure, quiet)
         call halt_on_failure(stat)
     end subroutine assert_equal_user_defined
+
+    !>実測値`actual`と予測値`expected`の全要素の等値性を比較する．
+    !>比較結果が偽であれば，プログラムの実行を停止する．
+    !>比較には，手続`comparator`が用いられる．
+    !>
+    !>`verbose`が真であれば，実測値と予測値を出力する．
+    !>実測値と予測値の出力には，手続`verbose_message_writer`
+    !>が用いられる．
+    !>
+    !>`expected_failure`が真の場合，比較が失敗することを検査し，
+    !>プログラムを停止しない．比較が成功すると停止する．
+    !>
+    !>`quiet`が真の場合，成功時の出力を抑制する．
+    !>
+    subroutine assert_equal_user_defined_rank1(actual, expected, test_name, &
+                                               comparator, verbose_message_writer, &
+                                               verbose, expected_failure, quiet)
+        use :: fassert_interface
+        class(*), intent(in) :: actual(:)
+            !! 実測値
+        class(*), intent(in) :: expected(:)
+            !! 予測値
+        character(*), intent(in) :: test_name
+            !! テスト名
+        procedure(Iis_equal_rank1) :: comparator
+            !! ユーザ定義型変数`actual`, `expected`の等価演算を行う手続
+        procedure(Ioutput_on_failure_to_unit_rank1), optional :: verbose_message_writer
+            !! 値の出力を行う手続
+        logical, intent(in), optional :: verbose
+            !! 実測値と予測値を出力するフラグ
+        logical, intent(in), optional :: expected_failure
+            !! 予期された失敗を検査するかのフラグ
+        logical, intent(in), optional :: quiet
+            !! 成功時に出力を抑制するかのフラグ
+
+        logical :: stat
+
+        call expect_equal(actual, expected, test_name, stat, &
+                          comparator, verbose_message_writer, &
+                          verbose, expected_failure, quiet)
+        call halt_on_failure(stat)
+    end subroutine assert_equal_user_defined_rank1
+
+    !>実測値`actual`と予測値`expected`の全要素の等値性を比較する．
+    !>比較結果が偽であれば，プログラムの実行を停止する．
+    !>比較には，手続`comparator`が用いられる．
+    !>
+    !>`verbose`が真であれば，実測値と予測値を出力する．
+    !>実測値と予測値の出力には，手続`verbose_message_writer`
+    !>が用いられる．
+    !>
+    !>`expected_failure`が真の場合，比較が失敗することを検査し，
+    !>プログラムを停止しない．比較が成功すると停止する．
+    !>
+    !>`quiet`が真の場合，成功時の出力を抑制する．
+    !>
+    subroutine assert_equal_user_defined_rank2(actual, expected, test_name, &
+                                               comparator, verbose_message_writer, &
+                                               verbose, expected_failure, quiet)
+        use :: fassert_interface
+        class(*), intent(in) :: actual(:, :)
+            !! 実測値
+        class(*), intent(in) :: expected(:, :)
+            !! 予測値
+        character(*), intent(in) :: test_name
+            !! テスト名
+        procedure(Iis_equal_rank2) :: comparator
+            !! ユーザ定義型変数`actual`, `expected`の等価演算を行う手続
+        procedure(Ioutput_on_failure_to_unit_rank2), optional :: verbose_message_writer
+            !! 値の出力を行う手続
+        logical, intent(in), optional :: verbose
+            !! 実測値と予測値を出力するフラグ
+        logical, intent(in), optional :: expected_failure
+            !! 予期された失敗を検査するかのフラグ
+        logical, intent(in), optional :: quiet
+            !! 成功時に出力を抑制するかのフラグ
+
+        logical :: stat
+
+        call expect_equal(actual, expected, test_name, stat, &
+                          comparator, verbose_message_writer, &
+                          verbose, expected_failure, quiet)
+        call halt_on_failure(stat)
+    end subroutine assert_equal_user_defined_rank2
+
+    !>実測値`actual`と予測値`expected`の全要素の等値性を比較する．
+    !>比較結果が偽であれば，プログラムの実行を停止する．
+    !>比較には，手続`comparator`が用いられる．
+    !>
+    !>`verbose`が真であれば，実測値と予測値を出力する．
+    !>実測値と予測値の出力には，手続`verbose_message_writer`
+    !>が用いられる．
+    !>
+    !>`expected_failure`が真の場合，比較が失敗することを検査し，
+    !>プログラムを停止しない．比較が成功すると停止する．
+    !>
+    !>`quiet`が真の場合，成功時の出力を抑制する．
+    !>
+    subroutine assert_equal_user_defined_rank3(actual, expected, test_name, &
+                                               comparator, verbose_message_writer, &
+                                               verbose, expected_failure, quiet)
+        use :: fassert_interface
+        class(*), intent(in) :: actual(:, :, :)
+            !! 実測値
+        class(*), intent(in) :: expected(:, :, :)
+            !! 予測値
+        character(*), intent(in) :: test_name
+            !! テスト名
+        procedure(Iis_equal_rank3) :: comparator
+            !! ユーザ定義型変数`actual`, `expected`の等価演算を行う手続
+        procedure(Ioutput_on_failure_to_unit_rank3), optional :: verbose_message_writer
+            !! 値の出力を行う手続
+        logical, intent(in), optional :: verbose
+            !! 実測値と予測値を出力するフラグ
+        logical, intent(in), optional :: expected_failure
+            !! 予期された失敗を検査するかのフラグ
+        logical, intent(in), optional :: quiet
+            !! 成功時に出力を抑制するかのフラグ
+
+        logical :: stat
+
+        call expect_equal(actual, expected, test_name, stat, &
+                          comparator, verbose_message_writer, &
+                          verbose, expected_failure, quiet)
+        call halt_on_failure(stat)
+    end subroutine assert_equal_user_defined_rank3
 end module assertEqual
