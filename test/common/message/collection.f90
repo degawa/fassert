0d96509915a4dbd147087bbbcc330a25fd74df6e
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-03-27 21:47:11 +0900

    refactored tests

--- test/common/message/collection.f90
+++ test/common/message/collection.f90
@@ -14,26 +14,14 @@ contains
             !! collection of tests
 
         test_suite = [ &
-                     new_unittest("to_string_logical(), it should return 'T' when input `.true.`.", &
-                                  toString_should_return_T_when_input_true) &
-                     , new_unittest("to_string_logical(), it should return 'F' when input `.false.`.", &
-                                    toString_should_return_F_when_input_false) &
-                     , new_unittest("does_output_message(), it should return `.true.` when input `.false.`.", &
-                                    doesOutputMsg_should_return_true_when_input_false) &
-                     , new_unittest("does_output_message(), it should return `.false.` when input `.true.`.", &
-                                    doesOutputMsg_should_return_false_when_input_true) &
-                     , new_unittest("does_output_message(), it should return `.true.` when no input.", &
-                                    doesOutputMsg_should_return_true_when_no_input) &
-                     , new_unittest("does_not_output_message(), it should return `.true.` when input `.true.`.", &
-                                    doesNotOutputMsg_should_return_true_when_input_true) &
-                     , new_unittest("does_not_output_message(), it should return `.false.` when input `.false.`.", &
-                                    doesNotOutputMsg_should_return_false_when_input_false) &
-                     , new_unittest("does_not_output_message(), it should return `.false.` when no input.", &
-                                    doesNotOutputMsg_should_return_false_when_no_input) &
-                     , new_unittest("is_verbose_output(), it should return `.true.` when input parameters.", &
-                                    isVerboseOutput_should_return_true_when_input_parameters) &
-                     , new_unittest("is_verbose_output(), it should return `.false.` when input parameters.", &
-                                    isVerboseOutput_should_return_false_when_input_parameters) &
+                     new_unittest("to_string_logical(), parameterized test.", &
+                                  toString_parameterized_test) &
+                     , new_unittest("does_output_message(), parameterized test.", &
+                                    doesOutputMsg_parameterized_test) &
+                     , new_unittest("does_not_output_message(), parameterized test.", &
+                                    doesNotOutputMsg_parameterized_test) &
+                     , new_unittest("is_verbose_output(), parameterized test.", &
+                                    isVerboseOutput_parameterized_test) &
                      , new_unittest("write_message_to_string(), it should write `message` to `output_message` "// &
                                     "when `message` is allocated.", &
                                     writeMsgToString_should_write_msg_to_str_when_allocated) &
--- test/common/message/unitTests/to_string.f90
+++ test/common/message/unitTests/to_string.f90
@@ -4,29 +4,69 @@ module test_common_message_unitTests_toString
     use :: fassert_common_message
     implicit none
     private
-    public :: toString_should_return_T_when_input_true
-    public :: toString_should_return_F_when_input_false
+    public :: toString_parameterized_test
 
 contains
-    subroutine toString_should_return_T_when_input_true(error)
+    subroutine toString_parameterized_test(error)
+        use :: par_funnel
+        use :: fassert_common_unit
         implicit none
         type(error_type), allocatable, intent(out) :: error
             !! error handler
 
-        call check(error, to_string(.true.) == string_true, &
-                   "expected "//string_true &
-                   //", but got "//to_string(.true.))
-        if (occurred(error)) return
-    end subroutine toString_should_return_T_when_input_true
+        type(test_parameter_type), allocatable :: params(:)
+        type(test_results_type) :: results
 
-    subroutine toString_should_return_F_when_input_false(error)
-        implicit none
-        type(error_type), allocatable, intent(out) :: error
-            !! error handler
+        params = [ &
+                   new_test_parameter(arguments="val=false", expected="retval='F'") &
+                 , new_test_parameter(arguments="val=true" , expected="retval='T'") &
+                 ] !&
 
-        call check(error, to_string(.false.) == string_false, &
-                   "expected "//string_false &
-                   //", but got "//to_string(.false.))
-        if (occurred(error)) return
-    end subroutine toString_should_return_F_when_input_false
+        call run_test_cases(params, results)
+        call check(error, results%all_cases_successful(), results%get_summary_message())
+    contains
+        subroutine run_test_cases(params, results)
+            use, intrinsic :: iso_fortran_env
+            type(test_parameter_type), intent(in) :: params(:)
+            type(test_results_type), intent(inout) :: results
+
+            logical :: val
+            character(1) :: retval
+
+            namelist /arguments/ val
+            namelist /expected/ retval
+
+            call results%construct(params)
+
+            block
+                character(:), allocatable :: test_name
+                integer(int32) :: case
+                character(1) :: actual
+
+                do case = 1, results%get_number_of_test_cases()
+                    read (unit=params(case)%arguments_namelist, nml=arguments)
+                    read (unit=params(case)%expected_namelist, nml=expected)
+
+                    test_name = "it should return "//params(case)%expected()// &
+                                " when input "//params(case)%arguments()
+                    write (output_unit, '(12X, "- ",A)') test_name
+
+                    actual = to_string(val)
+                    call results%check_test(case, actual == retval, &
+                                            failure_message(retval, actual, test_name))
+                end do
+            end block
+        end subroutine run_test_cases
+        function failure_message(expected, actual, test_name) result(msg)
+            implicit none
+            character(1), intent(in) :: expected
+            character(1), intent(in) :: actual
+            character(*), intent(in) :: test_name
+            character(:), allocatable :: msg
+
+            msg = test_name//new_line(" ")// &
+                  "    expected : "//expected//new_line(" ")// &
+                  "    actual   : "//actual
+        end function failure_message
+    end subroutine toString_parameterized_test
 end module test_common_message_unitTests_toString
