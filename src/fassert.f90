2ab44c38b9c444ff1d81a81ab0747ea9778dff1c
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-04-30 21:25:20 +0900

    refactored fassert module

--- src/fassert.f90
+++ src/fassert.f90
@@ -7,12 +7,15 @@ module fassert
     use :: expectLogical
     implicit none
     private
-    public :: set_assertion_message_unit
+    ! assert procedures
     public :: assert_equal
-    public :: expect_equal
-    public :: expect_same_shape
     public :: assert_true
     public :: assert_false
+    ! expect procedures
+    public :: expect_equal
     public :: expect_true
     public :: expect_false
+    public :: expect_same_shape
+    ! utility procedures
+    public :: set_assertion_message_unit
 end module fassert
