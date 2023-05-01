51a5c5ba447550c59f1dedad500773a5c43c18cb
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-05-01 16:20:31 +0900

    added comparator for int, real, complex arrays

--- src/common/compare/equal/array.f90
+++ src/common/compare/equal/array.f90
@@ -5,15 +5,36 @@ module fassert_common_compare_equal_array
     public :: are_equal
 
     interface are_equal
+        procedure :: are_all_values_equal_rank1_int8
+        procedure :: are_all_values_equal_rank2_int8
+        procedure :: are_all_values_equal_rank3_int8
+        procedure :: are_all_values_equal_rank1_int16
+        procedure :: are_all_values_equal_rank2_int16
+        procedure :: are_all_values_equal_rank3_int16
         procedure :: are_all_values_equal_rank1_int32
         procedure :: are_all_values_equal_rank2_int32
         procedure :: are_all_values_equal_rank3_int32
+        procedure :: are_all_values_equal_rank1_int64
+        procedure :: are_all_values_equal_rank2_int64
+        procedure :: are_all_values_equal_rank3_int64
         procedure :: are_all_values_equal_rank1_real32
         procedure :: are_all_values_equal_rank2_real32
         procedure :: are_all_values_equal_rank3_real32
         procedure :: are_all_values_equal_rank1_real64
         procedure :: are_all_values_equal_rank2_real64
         procedure :: are_all_values_equal_rank3_real64
+        procedure :: are_all_values_equal_rank1_real128
+        procedure :: are_all_values_equal_rank2_real128
+        procedure :: are_all_values_equal_rank3_real128
+        procedure :: are_all_values_equal_rank1_complex32
+        procedure :: are_all_values_equal_rank2_complex32
+        procedure :: are_all_values_equal_rank3_complex32
+        procedure :: are_all_values_equal_rank1_complex64
+        procedure :: are_all_values_equal_rank2_complex64
+        procedure :: are_all_values_equal_rank3_complex64
+        procedure :: are_all_values_equal_rank1_complex128
+        procedure :: are_all_values_equal_rank2_complex128
+        procedure :: are_all_values_equal_rank3_complex128
         procedure :: are_all_values_equal_rank1_logical
         procedure :: are_all_values_equal_rank2_logical
         procedure :: are_all_values_equal_rank3_logical
@@ -23,6 +44,66 @@ module fassert_common_compare_equal_array
     end interface
 
 contains
+    !>二つの配列の全要素が等しい場合に`.true.`，そうでない場合`.false.`を返す．
+    pure function are_all_values_equal_rank1_int8(array1, array2) result(are_same)
+        implicit none
+        integer(int8), intent(in) :: array1(:)
+        integer(int8), intent(in) :: array2(:)
+        logical :: are_same
+
+        are_same = all(array1 == array2)
+    end function are_all_values_equal_rank1_int8
+
+    !>二つの配列の全要素が等しい場合に`.true.`，そうでない場合`.false.`を返す．
+    pure function are_all_values_equal_rank2_int8(array1, array2) result(are_same)
+        implicit none
+        integer(int8), intent(in) :: array1(:, :)
+        integer(int8), intent(in) :: array2(:, :)
+        logical :: are_same
+
+        are_same = all(array1 == array2)
+    end function are_all_values_equal_rank2_int8
+
+    !>二つの配列の全要素が等しい場合に`.true.`，そうでない場合`.false.`を返す．
+    pure function are_all_values_equal_rank3_int8(array1, array2) result(are_same)
+        implicit none
+        integer(int8), intent(in) :: array1(:, :, :)
+        integer(int8), intent(in) :: array2(:, :, :)
+        logical :: are_same
+
+        are_same = all(array1 == array2)
+    end function are_all_values_equal_rank3_int8
+
+    !>二つの配列の全要素が等しい場合に`.true.`，そうでない場合`.false.`を返す．
+    pure function are_all_values_equal_rank1_int16(array1, array2) result(are_same)
+        implicit none
+        integer(int16), intent(in) :: array1(:)
+        integer(int16), intent(in) :: array2(:)
+        logical :: are_same
+
+        are_same = all(array1 == array2)
+    end function are_all_values_equal_rank1_int16
+
+    !>二つの配列の全要素が等しい場合に`.true.`，そうでない場合`.false.`を返す．
+    pure function are_all_values_equal_rank2_int16(array1, array2) result(are_same)
+        implicit none
+        integer(int16), intent(in) :: array1(:, :)
+        integer(int16), intent(in) :: array2(:, :)
+        logical :: are_same
+
+        are_same = all(array1 == array2)
+    end function are_all_values_equal_rank2_int16
+
+    !>二つの配列の全要素が等しい場合に`.true.`，そうでない場合`.false.`を返す．
+    pure function are_all_values_equal_rank3_int16(array1, array2) result(are_same)
+        implicit none
+        integer(int16), intent(in) :: array1(:, :, :)
+        integer(int16), intent(in) :: array2(:, :, :)
+        logical :: are_same
+
+        are_same = all(array1 == array2)
+    end function are_all_values_equal_rank3_int16
+
     !>二つの配列の全要素が等しい場合に`.true.`，そうでない場合`.false.`を返す．
     pure function are_all_values_equal_rank1_int32(array1, array2) result(are_same)
         implicit none
@@ -53,6 +134,36 @@ contains
         are_same = all(array1 == array2)
     end function are_all_values_equal_rank3_int32
 
+    !>二つの配列の全要素が等しい場合に`.true.`，そうでない場合`.false.`を返す．
+    pure function are_all_values_equal_rank1_int64(array1, array2) result(are_same)
+        implicit none
+        integer(int64), intent(in) :: array1(:)
+        integer(int64), intent(in) :: array2(:)
+        logical :: are_same
+
+        are_same = all(array1 == array2)
+    end function are_all_values_equal_rank1_int64
+
+    !>二つの配列の全要素が等しい場合に`.true.`，そうでない場合`.false.`を返す．
+    pure function are_all_values_equal_rank2_int64(array1, array2) result(are_same)
+        implicit none
+        integer(int64), intent(in) :: array1(:, :)
+        integer(int64), intent(in) :: array2(:, :)
+        logical :: are_same
+
+        are_same = all(array1 == array2)
+    end function are_all_values_equal_rank2_int64
+
+    !>二つの配列の全要素が等しい場合に`.true.`，そうでない場合`.false.`を返す．
+    pure function are_all_values_equal_rank3_int64(array1, array2) result(are_same)
+        implicit none
+        integer(int64), intent(in) :: array1(:, :, :)
+        integer(int64), intent(in) :: array2(:, :, :)
+        logical :: are_same
+
+        are_same = all(array1 == array2)
+    end function are_all_values_equal_rank3_int64
+
     !>二つの配列の全要素の差が許容値以下の場合に`.true.`，そうでない場合`.false.`を返す．
     !>許容値が指定されない場合，マシンイプシロンが許容値として使われる．
     pure function are_all_values_equal_rank1_real32(array1, array2, tolerance) result(are_same)
@@ -64,7 +175,7 @@ contains
         logical :: are_same
 
         are_same = all(abs(array1 - array2) <= optval(tolerance, &
-                                                      default=epsilon(array1)))
+                                                      default=epsilon(0._real32)))
     end function are_all_values_equal_rank1_real32
 
     !>二つの配列の全要素の差が許容値以下の場合に`.true.`，そうでない場合`.false.`を返す．
@@ -78,7 +189,7 @@ contains
         logical :: are_same
 
         are_same = all(abs(array1 - array2) <= optval(tolerance, &
-                                                      default=epsilon(array1)))
+                                                      default=epsilon(0._real32)))
     end function are_all_values_equal_rank2_real32
 
     !>二つの配列の全要素の差が許容値以下の場合に`.true.`，そうでない場合`.false.`を返す．
@@ -92,7 +203,7 @@ contains
         logical :: are_same
 
         are_same = all(abs(array1 - array2) <= optval(tolerance, &
-                                                      default=epsilon(array1)))
+                                                      default=epsilon(0._real32)))
     end function are_all_values_equal_rank3_real32
 
     !>二つの配列の全要素の差が許容値以下の場合に`.true.`，そうでない場合`.false.`を返す．
@@ -106,7 +217,7 @@ contains
         logical :: are_same
 
         are_same = all(abs(array1 - array2) <= optval(tolerance, &
-                                                      default=epsilon(array1)))
+                                                      default=epsilon(0._real64)))
     end function are_all_values_equal_rank1_real64
 
     !>二つの配列の全要素の差が許容値以下の場合に`.true.`，そうでない場合`.false.`を返す．
@@ -120,7 +231,7 @@ contains
         logical :: are_same
 
         are_same = all(abs(array1 - array2) <= optval(tolerance, &
-                                                      default=epsilon(array1)))
+                                                      default=epsilon(0._real64)))
     end function are_all_values_equal_rank2_real64
 
     !>二つの配列の全要素の差が許容値以下の場合に`.true.`，そうでない場合`.false.`を返す．
@@ -134,8 +245,176 @@ contains
         logical :: are_same
 
         are_same = all(abs(array1 - array2) <= optval(tolerance, &
-                                                      default=epsilon(array1)))
+                                                      default=epsilon(0._real64)))
     end function are_all_values_equal_rank3_real64
+
+    !>二つの配列の全要素の差が許容値以下の場合に`.true.`，そうでない場合`.false.`を返す．
+    !>許容値が指定されない場合，マシンイプシロンが許容値として使われる．
+    pure function are_all_values_equal_rank1_real128(array1, array2, tolerance) result(are_same)
+        use :: fassert_common_optval
+        implicit none
+        real(real128), intent(in) :: array1(:)
+        real(real128), intent(in) :: array2(:)
+        real(real128), intent(in), optional :: tolerance
+        logical :: are_same
+
+        are_same = all(abs(array1 - array2) <= optval(tolerance, &
+                                                      default=epsilon(0._real128)))
+    end function are_all_values_equal_rank1_real128
+
+    !>二つの配列の全要素の差が許容値以下の場合に`.true.`，そうでない場合`.false.`を返す．
+    !>許容値が指定されない場合，マシンイプシロンが許容値として使われる．
+    pure function are_all_values_equal_rank2_real128(array1, array2, tolerance) result(are_same)
+        use :: fassert_common_optval
+        implicit none
+        real(real128), intent(in) :: array1(:, :)
+        real(real128), intent(in) :: array2(:, :)
+        real(real128), intent(in), optional :: tolerance
+        logical :: are_same
+
+        are_same = all(abs(array1 - array2) <= optval(tolerance, &
+                                                      default=epsilon(0._real128)))
+    end function are_all_values_equal_rank2_real128
+
+    !>二つの配列の全要素の差が許容値以下の場合に`.true.`，そうでない場合`.false.`を返す．
+    !>許容値が指定されない場合，マシンイプシロンが許容値として使われる．
+    pure function are_all_values_equal_rank3_real128(array1, array2, tolerance) result(are_same)
+        use :: fassert_common_optval
+        implicit none
+        real(real128), intent(in) :: array1(:, :, :)
+        real(real128), intent(in) :: array2(:, :, :)
+        real(real128), intent(in), optional :: tolerance
+        logical :: are_same
+
+        are_same = all(abs(array1 - array2) <= optval(tolerance, &
+                                                      default=epsilon(0._real128)))
+    end function are_all_values_equal_rank3_real128
+
+    !>二つの配列の全要素の差が許容値以下の場合に`.true.`，そうでない場合`.false.`を返す．
+    !>許容値が指定されない場合，マシンイプシロンが許容値として使われる．
+    pure function are_all_values_equal_rank1_complex32(array1, array2, tolerance) result(are_same)
+        use :: fassert_common_optval
+        implicit none
+        complex(real32), intent(in) :: array1(:)
+        complex(real32), intent(in) :: array2(:)
+        real(real32), intent(in), optional :: tolerance
+        logical :: are_same
+
+        are_same = all(abs(array1 - array2) <= optval(tolerance, &
+                                                      default=epsilon(0._real32)))
+    end function are_all_values_equal_rank1_complex32
+
+    !>二つの配列の全要素の差が許容値以下の場合に`.true.`，そうでない場合`.false.`を返す．
+    !>許容値が指定されない場合，マシンイプシロンが許容値として使われる．
+    pure function are_all_values_equal_rank2_complex32(array1, array2, tolerance) result(are_same)
+        use :: fassert_common_optval
+        implicit none
+        complex(real32), intent(in) :: array1(:, :)
+        complex(real32), intent(in) :: array2(:, :)
+        real(real32), intent(in), optional :: tolerance
+        logical :: are_same
+
+        are_same = all(abs(array1 - array2) <= optval(tolerance, &
+                                                      default=epsilon(0._real32)))
+    end function are_all_values_equal_rank2_complex32
+
+    !>二つの配列の全要素の差が許容値以下の場合に`.true.`，そうでない場合`.false.`を返す．
+    !>許容値が指定されない場合，マシンイプシロンが許容値として使われる．
+    pure function are_all_values_equal_rank3_complex32(array1, array2, tolerance) result(are_same)
+        use :: fassert_common_optval
+        implicit none
+        complex(real32), intent(in) :: array1(:, :, :)
+        complex(real32), intent(in) :: array2(:, :, :)
+        real(real32), intent(in), optional :: tolerance
+        logical :: are_same
+
+        are_same = all(abs(array1 - array2) <= optval(tolerance, &
+                                                      default=epsilon(0._real32)))
+    end function are_all_values_equal_rank3_complex32
+
+    !>二つの配列の全要素の差が許容値以下の場合に`.true.`，そうでない場合`.false.`を返す．
+    !>許容値が指定されない場合，マシンイプシロンが許容値として使われる．
+    pure function are_all_values_equal_rank1_complex64(array1, array2, tolerance) result(are_same)
+        use :: fassert_common_optval
+        implicit none
+        complex(real64), intent(in) :: array1(:)
+        complex(real64), intent(in) :: array2(:)
+        real(real64), intent(in), optional :: tolerance
+        logical :: are_same
+
+        are_same = all(abs(array1 - array2) <= optval(tolerance, &
+                                                      default=epsilon(0._real64)))
+    end function are_all_values_equal_rank1_complex64
+
+    !>二つの配列の全要素の差が許容値以下の場合に`.true.`，そうでない場合`.false.`を返す．
+    !>許容値が指定されない場合，マシンイプシロンが許容値として使われる．
+    pure function are_all_values_equal_rank2_complex64(array1, array2, tolerance) result(are_same)
+        use :: fassert_common_optval
+        implicit none
+        complex(real64), intent(in) :: array1(:, :)
+        complex(real64), intent(in) :: array2(:, :)
+        real(real64), intent(in), optional :: tolerance
+        logical :: are_same
+
+        are_same = all(abs(array1 - array2) <= optval(tolerance, &
+                                                      default=epsilon(0._real64)))
+    end function are_all_values_equal_rank2_complex64
+
+    !>二つの配列の全要素の差が許容値以下の場合に`.true.`，そうでない場合`.false.`を返す．
+    !>許容値が指定されない場合，マシンイプシロンが許容値として使われる．
+    pure function are_all_values_equal_rank3_complex64(array1, array2, tolerance) result(are_same)
+        use :: fassert_common_optval
+        implicit none
+        complex(real64), intent(in) :: array1(:, :, :)
+        complex(real64), intent(in) :: array2(:, :, :)
+        real(real64), intent(in), optional :: tolerance
+        logical :: are_same
+
+        are_same = all(abs(array1 - array2) <= optval(tolerance, &
+                                                      default=epsilon(0._real64)))
+    end function are_all_values_equal_rank3_complex64
+
+    !>二つの配列の全要素の差が許容値以下の場合に`.true.`，そうでない場合`.false.`を返す．
+    !>許容値が指定されない場合，マシンイプシロンが許容値として使われる．
+    pure function are_all_values_equal_rank1_complex128(array1, array2, tolerance) result(are_same)
+        use :: fassert_common_optval
+        implicit none
+        complex(real128), intent(in) :: array1(:)
+        complex(real128), intent(in) :: array2(:)
+        real(real128), intent(in), optional :: tolerance
+        logical :: are_same
+
+        are_same = all(abs(array1 - array2) <= optval(tolerance, &
+                                                      default=epsilon(0._real128)))
+    end function are_all_values_equal_rank1_complex128
+
+    !>二つの配列の全要素の差が許容値以下の場合に`.true.`，そうでない場合`.false.`を返す．
+    !>許容値が指定されない場合，マシンイプシロンが許容値として使われる．
+    pure function are_all_values_equal_rank2_complex128(array1, array2, tolerance) result(are_same)
+        use :: fassert_common_optval
+        implicit none
+        complex(real128), intent(in) :: array1(:, :)
+        complex(real128), intent(in) :: array2(:, :)
+        real(real128), intent(in), optional :: tolerance
+        logical :: are_same
+
+        are_same = all(abs(array1 - array2) <= optval(tolerance, &
+                                                      default=epsilon(0._real128)))
+    end function are_all_values_equal_rank2_complex128
+
+    !>二つの配列の全要素の差が許容値以下の場合に`.true.`，そうでない場合`.false.`を返す．
+    !>許容値が指定されない場合，マシンイプシロンが許容値として使われる．
+    pure function are_all_values_equal_rank3_complex128(array1, array2, tolerance) result(are_same)
+        use :: fassert_common_optval
+        implicit none
+        complex(real128), intent(in) :: array1(:, :, :)
+        complex(real128), intent(in) :: array2(:, :, :)
+        real(real128), intent(in), optional :: tolerance
+        logical :: are_same
+
+        are_same = all(abs(array1 - array2) <= optval(tolerance, &
+                                                      default=epsilon(0._real128)))
+    end function are_all_values_equal_rank3_complex128
 
     !>二つの配列の全要素の差が許容値以下の場合に`.true.`，そうでない場合`.false.`を返す．
     !>許容値が指定されない場合，マシンイプシロンが許容値として使われる．
