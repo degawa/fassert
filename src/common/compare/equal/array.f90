1b0197181f37b517b6ab9550303a026c02ebb327
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-05-01 13:45:23 +0900

    added comparator for logical arrays

--- src/common/compare/equal/array.f90
+++ src/common/compare/equal/array.f90
@@ -14,6 +14,9 @@ module fassert_common_compare_equal_array
         procedure :: are_all_values_equal_rank1_real64
         procedure :: are_all_values_equal_rank2_real64
         procedure :: are_all_values_equal_rank3_real64
+        procedure :: are_all_values_equal_rank1_logical
+        procedure :: are_all_values_equal_rank2_logical
+        procedure :: are_all_values_equal_rank3_logical
         procedure :: are_all_values_equal_rank1_char
     end interface
 
@@ -131,6 +134,42 @@ contains
         are_same = all(abs(array1 - array2) <= optval(tolerance, &
                                                       default=epsilon(array1)))
     end function are_all_values_equal_rank3_real64
+
+    !>二つの配列の全要素の差が許容値以下の場合に`.true.`，そうでない場合`.false.`を返す．
+    !>許容値が指定されない場合，マシンイプシロンが許容値として使われる．
+    pure function are_all_values_equal_rank1_logical(array1, array2) result(are_same)
+        use :: fassert_common_optval
+        implicit none
+        logical, intent(in) :: array1(:)
+        logical, intent(in) :: array2(:)
+        logical :: are_same
+
+        are_same = all(array1 .eqv. array2)
+    end function are_all_values_equal_rank1_logical
+
+    !>二つの配列の全要素の差が許容値以下の場合に`.true.`，そうでない場合`.false.`を返す．
+    !>許容値が指定されない場合，マシンイプシロンが許容値として使われる．
+    pure function are_all_values_equal_rank2_logical(array1, array2) result(are_same)
+        use :: fassert_common_optval
+        implicit none
+        logical, intent(in) :: array1(:, :)
+        logical, intent(in) :: array2(:, :)
+        logical :: are_same
+
+        are_same = all(array1 .eqv. array2)
+    end function are_all_values_equal_rank2_logical
+
+    !>二つの配列の全要素の差が許容値以下の場合に`.true.`，そうでない場合`.false.`を返す．
+    !>許容値が指定されない場合，マシンイプシロンが許容値として使われる．
+    pure function are_all_values_equal_rank3_logical(array1, array2) result(are_same)
+        use :: fassert_common_optval
+        implicit none
+        logical, intent(in) :: array1(:, :, :)
+        logical, intent(in) :: array2(:, :, :)
+        logical :: are_same
+
+        are_same = all(array1 .eqv. array2)
+    end function are_all_values_equal_rank3_logical
 
     !>二つの文字型配列の全要素が等しい場合に`.true.`，そうでない場合`.false.`を返す．
     pure function are_all_values_equal_rank1_char(array1, array2) result(are_same)
