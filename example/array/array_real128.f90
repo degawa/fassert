52413c3ffe396da5ad7f0e684ef556f3ef0edb6e
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-05-01 16:39:42 +0900

    fixed examples

--- example/array/array_real128.f90
+++ example/array/array_real128.f90
@@ -18,8 +18,8 @@ program array_real
     ! call expect_equal([real(real128) :: 1, 2, 3, 4, 5], [real(real128) :: 0, 0, 0, 0, 0], &
     !                   "result should be [0. 0. 0. 0. 0.] when input [1. 2. 3. 4. 5.]", stat)
     ! ! FAILED: result should be [0. 0. 0. 0. 0.] when input [1. 2. 3. 4. 5.]
-    ! !     Maximum Absolute Difference: 5.00000000 at (5)
-    ! !     Minimum Absolute Difference: 1.00000000 at (1)
+    ! !     Maximum Absolute Difference: .50000000000000000000000000000000E+00001 at (5)
+    ! !     Minimum Absolute Difference: .10000000000000000000000000000000E+00001 at (1)
     !```
 
     call expect_equal(reshape([real(real128) :: 0, 0, 0, 0, 0, 0], [2, 3]), &
@@ -39,7 +39,7 @@ program array_real
     !                   reshape([real(real128) :: 0, 0, 0, 0, 0, 0], [2, 3]), &
     !                   "result should be [[0. 0.] [0. 0.] [0. 0.]] when input [[1. 2.] [3. 4.] [5. 6.]]", stat)
     ! ! FAILED: result should be [[0. 0.] [0. 0.] [0. 0.]] when input [[1. 2.] [3. 4.] [5. 6.]]
-    ! !     Maximum Absolute Difference: 6.00000000 at (2,3)
-    ! !     Minimum Absolute Difference: 1.00000000 at (1,1)
+    ! !     Maximum Absolute Difference: .60000000000000000000000000000000E+00001 at (2,3)
+    ! !     Minimum Absolute Difference: .10000000000000000000000000000000E+00001 at (1,1)
     !```
 end program array_real
--- example/array/array_real64.f90
+++ example/array/array_real64.f90
@@ -18,8 +18,8 @@ program array_real
     ! call expect_equal([real(real64) :: 1, 2, 3, 4, 5], [real(real64) :: 0, 0, 0, 0, 0], &
     !                   "result should be [0. 0. 0. 0. 0.] when input [1. 2. 3. 4. 5.]", stat)
     ! ! FAILED: result should be [0. 0. 0. 0. 0.] when input [1. 2. 3. 4. 5.]
-    ! !     Maximum Absolute Difference: 5.00000000 at (5)
-    ! !     Minimum Absolute Difference: 1.00000000 at (1)
+    ! !     Maximum Absolute Difference:  0.500000000000000E+001 at (5)
+    ! !     Minimum Absolute Difference:  0.100000000000000E+001 at (1)
     !```
 
     call expect_equal(reshape([real(real64) :: 0, 0, 0, 0, 0, 0], [2, 3]), &
@@ -39,7 +39,7 @@ program array_real
     !                   reshape([real(real64) :: 0, 0, 0, 0, 0, 0], [2, 3]), &
     !                   "result should be [[0. 0.] [0. 0.] [0. 0.]] when input [[1. 2.] [3. 4.] [5. 6.]]", stat)
     ! ! FAILED: result should be [[0. 0.] [0. 0.] [0. 0.]] when input [[1. 2.] [3. 4.] [5. 6.]]
-    ! !     Maximum Absolute Difference: 6.00000000 at (2,3)
-    ! !     Minimum Absolute Difference: 1.00000000 at (1,1)
+    ! !     Maximum Absolute Difference:  0.600000000000000E+001 at (2,3)
+    ! !     Minimum Absolute Difference:  0.100000000000000E+001 at (1,1)
     !```
 end program array_real
