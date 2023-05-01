3ec9fc7035206c493f77bfb6b90c06a47b9eb93f
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-05-01 12:37:57 +0900

    added complex(real128) to comparator

--- src/common/compare/equal/complex.f90
+++ src/common/compare/equal/complex.f90
@@ -7,6 +7,7 @@ module fassert_common_compare_equal_complex
     interface is_approx_equal
         procedure :: is_approx_equal_complex32
         procedure :: is_approx_equal_complex64
+        procedure :: is_approx_equal_complex128
     end interface
 
 contains
@@ -37,4 +38,18 @@ contains
         is_equal = (abs(val1 - val2) <= optval(tolerance, &
                                                default=epsilon(0._real64)))
     end function is_approx_equal_complex64
+
+    !>二つのスカラ値の差が許容値以下の場合に`.true.`，そうでない場合`.false.`を返す．
+    !>許容値が指定されない場合，マシンイプシロンが許容値として使われる．
+    pure function is_approx_equal_complex128(val1, val2, tolerance) result(is_equal)
+        use :: fassert_common_optval
+        implicit none
+        complex(real128), intent(in) :: val1
+        complex(real128), intent(in) :: val2
+        real(real128), intent(in), optional :: tolerance
+        logical :: is_equal
+
+        is_equal = (abs(val1 - val2) <= optval(tolerance, &
+                                               default=epsilon(0._real128)))
+    end function is_approx_equal_complex128
 end module fassert_common_compare_equal_complex
