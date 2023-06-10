73b20a69e7f25c54f39a134216c4be6b8d10b7d8
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-06-10 17:01:45 +0900

    added approx module

---
+++ src/common/compare/approx.f90
@@ -0,0 +1,7 @@
+module fassert_common_compare_approx
+    use :: fassert_common_compare_approx_array
+    use :: fassert_common_compare_approx_real
+    use :: fassert_common_compare_approx_complex
+    implicit none
+    public
+end module fassert_common_compare_approx
