2001f2a982e68250c3e109b58d779e1ce36eaa8b
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-06-29 17:59:04 +0900

    added tests for fp_real64

--- test/common/floatingPointNumber/collection.f90
+++ test/common/floatingPointNumber/collection.f90
@@ -2,6 +2,7 @@ module test_common_floatingPointNumber_collection
     use :: testdrive, only:new_unittest, unittest_type, to_string
     use :: fassert_common_userSpecified
     use :: test_common_floatingPointNumber_unitTests_real32
+    use :: test_common_floatingPointNumber_unitTests_real64
     implicit none
     private
     public :: collect_fp
@@ -20,6 +21,13 @@ contains
                                     sign_real32_returns_0_when_input_positive_number) &
                      , new_unittest("sign_real32(), it should return 1 when input a negative number", &
                                     sign_real32_returns_1_when_input_negative_number) &
+                     , new_unittest("is_distance_less_than_n_ulp_real64(), it should return true when 2 values are within "// &
+                                    to_string(ULP)//" ulp.", &
+                                    is_distance_less_than_n_ulp_real64_returns_true) &
+                     , new_unittest("sign_real64(), it should return 0 when input a positive number", &
+                                    sign_real64_returns_0_when_input_positive_number) &
+                     , new_unittest("sign_real64(), it should return 1 when input a negative number", &
+                                    sign_real64_returns_1_when_input_negative_number) &
                      ]
     end subroutine collect_fp
 end module test_common_floatingPointNumber_collection
