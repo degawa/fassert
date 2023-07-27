3bb5c90f2ba90ee2b1be1ad54cf562c78802b022
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-07-27 21:43:19 +0900

    renamed preprocessor macro

--- src/common/userSpecified.f90
+++ src/common/userSpecified.f90
@@ -3,10 +3,10 @@ module fassert_common_userSpecified
     implicit none
     private
 
-#if !defined (_ULP) || (defined _ULP && _ULP<1)
-#define _ULP 2
+#if !defined (FASSERT_ULP) || (defined FASSERT_ULP && FASSERT_ULP<1)
+#define FASSERT_ULP 2
 #endif
-    integer(int32), public, parameter :: ULP = _ULP
+    integer(int32), public, parameter :: ULP = FASSERT_ULP
         !! 実数の等値性を判定するULPの差<br>
         !! 1以上でなければならない
 end module fassert_common_userSpecified
