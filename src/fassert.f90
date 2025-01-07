f0d768ebbc9c61df19057ad4eb824b86fb88429a
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2025-01-07 14:40:21 +0900

    updated fassert module

--- src/fassert.f90
+++ src/fassert.f90
@@ -9,6 +9,8 @@ module fassert
     use :: expectLogical
     use :: expectCharEqual
     use :: assertCharEqual
+    use :: expectSameType
+    use :: assertSameType
     implicit none
     private
     ! assert procedures
@@ -17,12 +19,14 @@ module fassert
     public :: assert_true
     public :: assert_false
     public :: assert_char_equal
+    public :: assert_same_type
     ! expect procedures
     public :: expect_equal
     public :: expect_approx_equal
     public :: expect_true
     public :: expect_false
     public :: expect_char_equal
+    public :: expect_same_type
     public :: expect_same_shape
     ! utility procedures
     public :: set_assertion_message_unit
